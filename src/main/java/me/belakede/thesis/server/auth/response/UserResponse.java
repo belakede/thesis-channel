59e80c0227aff5c5eff2d10b04ba385bae6f5e07
Adam Belak <belakede@gmail.com>
2016-10-27 14:59:29 +0200

    Add and test UserResponse

---
+++ src/main/java/me/belakede/thesis/server/auth/response/UserResponse.java
@@ -0,0 +1,50 @@
+package me.belakede.thesis.server.auth.response;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+
+@XmlRootElement
+public class UserResponse implements Serializable {
+
+    private static final long serialVersionUID = -6329451142194966807L;
+
+    @XmlElement(required = true)
+    private String username;
+
+    public UserResponse() {
+    }
+
+    public UserResponse(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        UserResponse that = (UserResponse) o;
+
+        return username != null ? username.equals(that.username) : that.username == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return username != null ? username.hashCode() : 0;
+    }
+
+
+}
---
+++ src/test/java/me/belakede/thesis/server/auth/response/UserResponseTest.java
@@ -0,0 +1,40 @@
+package me.belakede.thesis.server.auth.response;
+
+import me.belakede.thesis.junit.ExtendedEqualsHashCodeTestCase;
+import me.belakede.thesis.junit.PojoClassTestCase;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.*;
+
+@RunWith(Enclosed.class)
+public class UserResponseTest {
+
+    public static final class UserResponseEqualsAndHashCodeTest extends ExtendedEqualsHashCodeTestCase {
+
+        public UserResponseEqualsAndHashCodeTest(String name) {
+            super(name, UserResponse.class, Arrays.asList());
+        }
+
+        @Override
+        protected UserResponse createInstance() throws Exception {
+            return new UserResponse("admin");
+        }
+
+        @Override
+        protected UserResponse createNotEqualInstance() throws Exception {
+            return new UserResponse("demo");
+        }
+    }
+
+    public static final class UserResponsePojoTest extends PojoClassTestCase {
+
+        public UserResponsePojoTest(String name) {
+            super(name, UserResponse.class);
+        }
+    }
+
+}
