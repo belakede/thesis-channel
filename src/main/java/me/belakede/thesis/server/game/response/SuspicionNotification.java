f6ebb68ee68e26a1715b760676bcc414aff76131
Adam Belak <belakede@gmail.com>
2016-11-02 14:13:27 +0100

    Add SuspicionNotification

---
+++ src/main/java/me/belakede/thesis/server/game/response/SuspicionNotification.java
@@ -0,0 +1,27 @@
+package me.belakede.thesis.server.game.response;
+
+import me.belakede.thesis.game.equipment.Room;
+import me.belakede.thesis.game.equipment.Suspect;
+import me.belakede.thesis.game.equipment.Weapon;
+import me.belakede.thesis.server.Suspicion;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+
+@XmlRootElement
+public class SuspicionNotification extends Suspicion implements Serializable {
+    private static final long serialVersionUID = -530663703588895110L;
+
+    public SuspicionNotification() {
+        // It's required for an entity
+    }
+
+    public SuspicionNotification(me.belakede.thesis.game.equipment.Suspicion suspicion) {
+        super(suspicion);
+    }
+
+    public SuspicionNotification(Suspect suspect, Room room, Weapon weapon) {
+        super(suspect, room, weapon);
+    }
+
+}
---
+++ src/test/java/me/belakede/thesis/server/game/response/SuspicionNotificationTest.java
@@ -0,0 +1,40 @@
+package me.belakede.thesis.server.game.response;
+
+import me.belakede.thesis.game.equipment.Room;
+import me.belakede.thesis.game.equipment.Suspect;
+import me.belakede.thesis.game.equipment.Weapon;
+import me.belakede.thesis.junit.ExtendedEqualsHashCodeTestCase;
+import me.belakede.thesis.junit.PojoClassTestCase;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+
+import java.util.Arrays;
+
+@RunWith(Enclosed.class)
+public class SuspicionNotificationTest {
+
+    public static final class SuspicionNotificationEqualsHashCodeTest extends ExtendedEqualsHashCodeTestCase {
+
+        public SuspicionNotificationEqualsHashCodeTest(String name) {
+            super(name, SuspicionNotification.class, Arrays.asList("suspect", "room", "weapon"));
+        }
+
+        @Override
+        protected SuspicionNotification createInstance() throws Exception {
+            return new SuspicionNotification(Suspect.GREEN, Room.BEDROOM, Weapon.LEAD_PIPE);
+        }
+
+        @Override
+        protected SuspicionNotification createNotEqualInstance() throws Exception {
+            return new SuspicionNotification(Suspect.MUSTARD, Room.KITCHEN, Weapon.KNIFE);
+        }
+    }
+
+    public static final class SuspicionNotificationPojoTest extends PojoClassTestCase {
+
+        public SuspicionNotificationPojoTest(String name) {
+            super(name, SuspicionNotification.class);
+        }
+    }
+
+}
