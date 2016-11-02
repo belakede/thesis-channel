772ef372cb521f2a191c45fd84802f2b9bdb2a4d
belakede <belakede@gmail.com>
2016-11-02 20:22:19 +0100

    Use JsonTypeInfo and JsonSubTypes instead of JsonDeserialize

--- src/main/java/me/belakede/thesis/server/game/response/Notification.java
+++ src/main/java/me/belakede/thesis/server/game/response/Notification.java
@@ -1,8 +1,17 @@
 package me.belakede.thesis.server.game.response;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import me.belakede.thesis.jackson.NotificationDeserializer;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
-@JsonDeserialize(using = NotificationDeserializer.class)
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = AccusationNotification.class, name = "accusation"),
+        @JsonSubTypes.Type(value = CardNotification.class, name = "card"),
+        @JsonSubTypes.Type(value = CurrentPlayerNotification.class, name = "player"),
+        @JsonSubTypes.Type(value = FigurineNotification.class, name = "figurine"),
+        @JsonSubTypes.Type(value = PairOfDiceNotification.class, name = "dice"),
+        @JsonSubTypes.Type(value = ShowYourCardNotification.class, name = "show"),
+        @JsonSubTypes.Type(value = SuspicionNotification.class, name = "suspicion")
+})
 public interface Notification {
 }
