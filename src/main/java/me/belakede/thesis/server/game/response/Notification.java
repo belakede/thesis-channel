4ab46e3d9171db87ec1d38503b44a83b109b10bb
Adam Belak <belakede@gmail.com>
2016-11-02 16:23:44 +0100

    Add JsonDeserialize using NotificationDeserializer

--- src/main/java/me/belakede/thesis/server/game/response/Notification.java
+++ src/main/java/me/belakede/thesis/server/game/response/Notification.java
@@ -1,5 +1,8 @@
 package me.belakede.thesis.server.game.response;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import me.belakede.thesis.jackson.NotificationDeserializer;
 
+@JsonDeserialize(using = NotificationDeserializer.class)
 public interface Notification {
 }
